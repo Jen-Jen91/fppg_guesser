d57d2b361b7cf9f804f60d7a9fe947b7d68cb5c8
Jen Proctor <jen_p_13@hotmail.co.uk>
2020-03-02 12:04:46 +0000

    Install Jest and add snapshot test to App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import renderer from "react-test-renderer";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("App snapshot test", () => {
+  const tree = renderer.create(<App />).toJSON();
+  console.log("TREE: ", tree);
+  expect(tree).toMatchSnapshot();
 });
